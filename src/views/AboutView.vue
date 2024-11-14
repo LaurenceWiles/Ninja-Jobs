029db1cbc22ee9ad001c2f0e6ec18a690708fa1f
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-11-14 12:59:34 +0100

    router links

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, hic
+      consequatur. Mollitia iure minus accusantium explicabo dolor pariatur ut
+      harum doloribus similique sed dolorum enim maiores assumenda eius possimus
+      est, esse inventore! Natus quos quasi quis dicta nobis illum corporis
+      distinctio? Commodi adipisci tempora distinctio eaque, unde at tempore
+      doloremque incidunt cupiditate perspiciatis ab sint et quaerat! Neque
+      delectus reiciendis voluptas veniam dolor rem! Possimus nobis, illum animi
+      consequatur est laborum placeat provident harum nam vel facilis saepe
+      ducimus, accusamus dolore quasi libero nisi praesentium architecto
+      expedita amet veniam quis. Impedit adipisci, officiis aliquam optio id vel
+      a mollitia illo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, hic
+      consequatur. Mollitia iure minus accusantium explicabo dolor pariatur ut
+      harum doloribus similique sed dolorum enim maiores assumenda eius possimus
+      est, esse inventore! Natus quos quasi quis dicta nobis illum corporis
+      distinctio? Commodi adipisci tempora distinctio eaque, unde at tempore
+      doloremque incidunt cupiditate perspiciatis ab sint et quaerat! Neque
+      delectus reiciendis voluptas veniam dolor rem! Possimus nobis, illum animi
+      consequatur est laborum placeat provident harum nam vel facilis saepe
+      ducimus, accusamus dolore quasi libero nisi praesentium architecto
+      expedita amet veniam quis. Impedit adipisci, officiis aliquam optio id vel
+      a mollitia illo.
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolore
+      quis molestiae temporibus impedit reprehenderit numquam autem aliquid
+      incidunt corrupti, quaerat veritatis ipsa voluptatem possimus et earum
+      vitae, accusantium ullam eaque quam tenetur aspernatur quibusdam
+      repellendus. Ut asperiores non minus quasi, velit rem nam natus dolores
+      laboriosam, sequi eos corporis pariatur quo ratione quae voluptate sint
+      dicta. Ut enim maxime qui, dolores beatae molestiae voluptates natus
+      ratione sequi delectus fugiat architecto? Magni harum sequi rerum nostrum,
+      quam deleniti eaque accusamus dolores blanditiis numquam temporibus autem
+      quisquam corrupti voluptate quis! Earum rem repudiandae iste repellat
+      commodi praesentium asperiores, illum ipsam alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolore
+      quis molestiae temporibus impedit reprehenderit numquam autem aliquid
+      incidunt corrupti, quaerat veritatis ipsa voluptatem possimus et earum
+      vitae, accusantium ullam eaque quam tenetur aspernatur quibusdam
+      repellendus. Ut asperiores non minus quasi, velit rem nam natus dolores
+      laboriosam, sequi eos corporis pariatur quo ratione quae voluptate sint
+      dicta. Ut enim maxime qui, dolores beatae molestiae voluptates natus
+      ratione sequi delectus fugiat architecto? Magni harum sequi rerum nostrum,
+      quam deleniti eaque accusamus dolores blanditiis numquam temporibus autem
+      quisquam corrupti voluptate quis! Earum rem repudiandae iste repellat
+      commodi praesentium asperiores, illum ipsam alias.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
